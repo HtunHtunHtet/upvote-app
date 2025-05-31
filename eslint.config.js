7ac57fde342b0cc13454f6166108b714b2154843
Htun Htun Htet <ryanhhh91@gmail.com>
2025-05-31 10:09:49 -0600

    lint fixes

--- eslint.config.js
+++ eslint.config.js
@@ -7,13 +7,84 @@ import markdown from "@eslint/markdown";
 import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
-  { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
-  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+  {
+    ignores: ["node_modules", "dist", ".idea"],
+  },
+
+  // JSON
+  {
+    files: ["**/*.json"],
+    ignores: ["package-lock.json"],
+    plugins: { json },
+    language: "json/json",
+    extends: ["json/recommended"],
+  },
+
+  // Markdown
+  {
+    files: ["**/*.md"],
+    plugins: { markdown },
+    language: "markdown/gfm",
+    extends: ["markdown/recommended"],
+  },
+
+  // CSS
+  {
+    files: ["**/*.css"],
+    plugins: { css },
+    language: "css/css",
+    extends: ["css/recommended"],
+  },
+
+  // React + TypeScript custom config
+  {
+    files: ["**/*.{js,ts,jsx,tsx}"],
+    ignores:["eslint.config.js"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: "./tsconfig.app.json",
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: globals.browser,
+    },
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+      react: pluginReact,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-filename-extension": ["warn", { extensions: [".tsx", ".jsx"] }],
+      "react/prop-types": "off",
+    },
+  },
+
+  // js.configs.recommended,
+  // tseslint.configs.recommended,
+  // pluginReact.configs.flat.recommended,
+
+
+  {
+    files: ["**/*.{js,cjs,mjs,ts,tsx,jsx}"],
+    ...js.configs.recommended[0],
+  },
+  {
+    files: ["**/*.{ts,tsx}"],
+    ...tseslint.configs.recommended[0],
+  },
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    ...pluginReact.configs.flat.recommended[0],
+  },
+
 ]);
